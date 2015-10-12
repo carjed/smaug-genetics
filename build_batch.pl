ab1b11e0d7bc1b721527fdf2f9a6a408c08f6c93
Jedidiah Carlson <jedidiah@umich.edu>
2015-10-12 14:53:32 -0400

    fix binwidth parameter in prediction script

--- build_batch.pl
+++ build_batch.pl
@@ -41,4 +41,4 @@ print OUT "#SBATCH --job-name=predict \n";
 print OUT "#SBATCH --partition=nomosix \n";
 print OUT "#SBATCH --array=$trainchr \n";
 print OUT "#SBATCH --output=\"/net/bipolar/jedidiah/mutation/output/slurm/slurmJob-%J.out\" --error=\"/net/bipolar/jedidiah/mutation/output/slurm/slurmJob-%J.err\" \n";
-print OUT "srun perl /net/bipolar/jedidiah/mutation/smaug-genetics/predict.pl --chr \$SLURM_ARRAY_TASK_ID --cat $cat --binw $binw \n";
+print OUT "srun perl /net/bipolar/jedidiah/mutation/smaug-genetics/predict.pl --chr \$SLURM_ARRAY_TASK_ID --cat $cat --binw 100000 \n";

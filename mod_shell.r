ac5aaa5f328c3a1c9db0d6991f59b2a5c90708da
Jedidiah Carlson <jedidiah@umich.edu>
2015-12-30 22:39:35 -0500

    7bp predictions

--- mod_shell.r
+++ mod_shell.r
@@ -37,10 +37,10 @@ args <- getArgs(
 		# binfile=paste0(parentdir, "/output/7bp_1000k/chrX.bin_out.txt"),
 		run_agg=TRUE,
 		pcs=FALSE,
-		categ="AT_CG",
+		categ="AT_GC",
 		negbin_model=TRUE,
-		log_model=FALSE,
-		run_predict=FALSE))
+		log_model=TRUE,
+		run_predict=TRUE))
 
 # The usePackage function loads packages if they already exist,
 # otherwise installs from default CRAN repository

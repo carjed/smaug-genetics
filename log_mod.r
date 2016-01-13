0707f60f84dbbefd606a92122c8e0d57b7027dae
Jedidiah Carlson <jedidiah@umich.edu>
2016-01-12 22:21:22 -0500

    run GC_AT

--- log_mod.r
+++ log_mod.r
@@ -70,7 +70,7 @@ nchr <- length(trainchr1)
 # fullfile <- paste0(parentdir, "/output/logmod_data/",categ,"_full.txt")
 # Only subset if specified temp file does not exist
 testfile <- paste0(parentdir, "/output/logmod_data/chr22/chr22_",
-	categ, "_TTTATTG(CAATAAA).txt")
+	categ, "_TTTCTTG(CAAGAAA).txt")
 if(!file.exists(testfile)){
 
 	modtime <- proc.time()

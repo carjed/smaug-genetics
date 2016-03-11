93f393e2796568a269f00a5e350b8acd8ba2b290
Jed Carlson <jedidiah@umich.edu>
2016-03-10 20:58:55 -0500

    add agg_mu.sh

--- agg_mu.sh
+++ agg_mu.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-paste /net/bipolar/jedidiah/mutation/output/predicted/chr$1_AT_CG_predicted.txt <(cut -d $'\t' -f3 /net/bipolar/jedidiah/mutation/output/predicted/chr$1_AT_GC_predicted.txt) > /net/bipolar/jedidiah/mutation/output/predicted/chr$1_tmp2.txt
-paste /net/bipolar/jedidiah/mutation/output/predicted/chr$1_tmp2.txt <(cut -d $'\t' -f3 /net/bipolar/jedidiah/mutation/output/predicted/chr$1_AT_TA_predicted.txt) | awk -v OFS='\t' '{print $1,$3+$4+$5}' > /net/bipolar/jedidiah/mutation/output/predicted/chr$1_AT.txt
+chr=$1
 
-paste /net/bipolar/jedidiah/mutation/output/predicted/chr$1_GC_AT_predicted.txt <(cut -d $'\t' -f3 /net/bipolar/jedidiah/mutation/output/predicted/chr$1_GC_CG_predicted.txt) > /net/bipolar/jedidiah/mutation/output/predicted/chr$1_tmp.txt
-paste /net/bipolar/jedidiah/mutation/output/predicted/chr$1_tmp.txt <(cut -d $'\t' -f3 /net/bipolar/jedidiah/mutation/output/predicted/chr$1_GC_TA_predicted.txt) | awk -v OFS='\t' '{print $1,$3+$4+$5}' > /net/bipolar/jedidiah/mutation/output/predicted/chr$1_GC.txt
+paste /net/bipolar/jedidiah/mutation/output/predicted/chr$chr_AT_CG_predicted.txt <(cut -d $'\t' -f3 /net/bipolar/jedidiah/mutation/output/predicted/chr$chr_AT_GC_predicted.txt) > /net/bipolar/jedidiah/mutation/output/predicted/chr$chr_tmp2.txt
+paste /net/bipolar/jedidiah/mutation/output/predicted/chr$chr_tmp2.txt <(cut -d $'\t' -f3 /net/bipolar/jedidiah/mutation/output/predicted/chr$chr_AT_TA_predicted.txt) | awk -v OFS='\t' '{print $1,$3+$4+$5}' > /net/bipolar/jedidiah/mutation/output/predicted/chr$chr_AT.txt
 
-cat /net/bipolar/jedidiah/mutation/output/predicted/chr$1_AT.txt /net/bipolar/jedidiah/mutation/output/predicted/chr$1_GC.txt | sort -n -k1,1 > /net/bipolar/jedidiah/mutation/output/predicted/chr$1_full.txt
+paste /net/bipolar/jedidiah/mutation/output/predicted/chr$chr_GC_AT_predicted.txt <(cut -d $'\t' -f3 /net/bipolar/jedidiah/mutation/output/predicted/chr$chr_GC_CG_predicted.txt) > /net/bipolar/jedidiah/mutation/output/predicted/chr$chr_tmp.txt
+paste /net/bipolar/jedidiah/mutation/output/predicted/chr$chr_tmp.txt <(cut -d $'\t' -f3 /net/bipolar/jedidiah/mutation/output/predicted/chr$chr_GC_TA_predicted.txt) | awk -v OFS='\t' '{print $1,$3+$4+$5}' > /net/bipolar/jedidiah/mutation/output/predicted/chr$chr_GC.txt
 
-rm -f /net/bipolar/jedidiah/mutation/output/predicted/chr$1_{AT,GC,tmp,tmp2}.txt
+cat /net/bipolar/jedidiah/mutation/output/predicted/chr$chr_AT.txt /net/bipolar/jedidiah/mutation/output/predicted/chr$chr_GC.txt | sort -n -k1,1 > /net/bipolar/jedidiah/mutation/output/predicted/chr$chr_full.txt
 
-awk -F$'\t' '{print ($1-1)"\t"$1"\t"$2}' /net/bipolar/jedidiah/mutation/output/predicted/chr$1_full.txt > /net/bipolar/jedidiah/mutation/output/predicted/chr$1_full.bed
+rm -f /net/bipolar/jedidiah/mutation/output/predicted/chr$chr_{AT,GC,tmp,tmp2}.txt
 
-bedtools intersect -a /net/bipolar/jedidiah/mutation/output/predicted/chr$1_full.bed -b /net/bipolar/jedidiah/mutation/reference_data/20140520.strict_mask2.bed  | awk '{print $2"\t"$3}' > /net/bipolar/jedidiah/mutation/output/predicted/chr$1_full_mask.txt
+awk -F$'\t' '{print $chr"\t"($1-1)"\t"$1"\t"$2}' /net/bipolar/jedidiah/mutation/output/predicted/chr$chr_full.txt > /net/bipolar/jedidiah/mutation/output/predicted/chr$chr_full.bed
+
+bedtools intersect -a /net/bipolar/jedidiah/mutation/output/predicted/chr$chr_full.bed -b /net/bipolar/jedidiah/mutation/reference_data/20140520.strict_mask2.bed  | awk '{print "\t"$3"\t"$4}' > /net/bipolar/jedidiah/mutation/output/predicted/chr$chr_full_mask.txt

ff70867521f33255b48f1f650ba1d88b43ab89a0
Jed Carlson <jedidiah@umich.edu>
2016-03-10 20:06:20 -0500

    add agg_mu.sh

---
+++ agg_mu.sh
@@ -0,0 +1,15 @@
+#!/bin/csh
+
+paste chr$1_AT_CG_predicted.txt <(cut -d $'\t' -f3 chr$1_AT_GC_predicted.txt) > chr$1_tmp2.txt
+paste chr$1_tmp2.txt <(cut -d $'\t' -f3 chr$1_AT_TA_predicted.txt) | awk -v OFS='\t' '{print $1,$3+$4+$5}' > chr$1_AT.txt
+
+paste chr$1_GC_AT_predicted.txt <(cut -d $'\t' -f3 chr$1_GC_CG_predicted.txt) > chr$1_tmp.txt
+paste chr$1_tmp.txt <(cut -d $'\t' -f3 chr$1_GC_TA_predicted.txt) | awk -v OFS='\t' '{print $1,$3+$4+$5}' > chr$1_GC.txt
+
+cat chr$1_AT.txt chr$1_GC.txt | sort -n -k1,1 > chr$1_full.txt
+
+rm -f chr$1_{AT,GC,tmp,tmp2}.txt
+
+awk -F$'\t' '{print ($1-1)"\t"$1"\t"$2}' /net/bipolar/jedidiah/mutation/output/predicted/chr$1_full.txt > /net/bipolar/jedidiah/mutation/output/predicted/chr$1_full.bed
+
+bedtools intersect -a /net/bipolar/jedidiah/mutation/output/predicted/chr$1_full.bed -b /net/bipolar/jedidiah/mutation/reference_data/20140520.strict_mask2.bed > /net/bipolar/jedidiah/mutation/output/predicted/chr$1_full_mask.txt

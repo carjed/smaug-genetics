ef84e742066e8c1834f4b1e3aecf39c95cdb54cd
Jed Carlson <jedidiah@umich.edu>
2016-05-01 19:02:16 -0400

    get full mu chr21

--- data_mgmt/agg_mu.sh
+++ data_mgmt/agg_mu.sh
@@ -8,11 +8,11 @@ paste /net/bipolar/jedidiah/mutation/output/predicted/chr${chr}_tmp2.txt <(cut -
 paste /net/bipolar/jedidiah/mutation/output/predicted/chr${chr}_GC_AT_predicted.txt <(cut -d $'\t' -f3 /net/bipolar/jedidiah/mutation/output/predicted/chr${chr}_GC_CG_predicted.txt) > /net/bipolar/jedidiah/mutation/output/predicted/chr${chr}_tmp.txt
 paste /net/bipolar/jedidiah/mutation/output/predicted/chr${chr}_tmp.txt <(cut -d $'\t' -f3 /net/bipolar/jedidiah/mutation/output/predicted/chr${chr}_GC_TA_predicted.txt) | awk -v OFS='\t' '{print $1,$3+$4+$5}' > /net/bipolar/jedidiah/mutation/output/predicted/chr${chr}_GC.txt
 
-cat /net/bipolar/jedidiah/mutation/output/predicted/chr${chr}_AT.txt /net/bipolar/jedidiah/mutation/output/predicted/chr${chr}_GC.txt | sort -n -k1,1 > /net/bipolar/jedidiah/mutation/output/predicted/chr${chr}_full.txt
+cat /net/bipolar/jedidiah/mutation/output/predicted/chr${chr}_AT.txt /net/bipolar/jedidiah/mutation/output/predicted/chr${chr}_GC.txt | sort -n -k1,1 > /net/bipolar/jedidiah/mutation/output/predicted/full/chr${chr}_full.txt
 
 rm -f /net/bipolar/jedidiah/mutation/output/predicted/chr${chr}_{AT,GC,tmp,tmp2}.txt
 
-awk -v chr="$chr" '{print chr"\t"($1-1)"\t"$1"\t"$2}' /net/bipolar/jedidiah/mutation/output/predicted/chr${chr}_full.txt > /net/bipolar/jedidiah/mutation/output/predicted/chr${chr}_full.bed
+# awk -v chr="$chr" '{print chr"\t"($1-1)"\t"$1"\t"$2}' /net/bipolar/jedidiah/mutation/output/predicted/chr${chr}_full.txt > /net/bipolar/jedidiah/mutation/output/predicted/chr${chr}_full.bed
 
 
-bedtools intersect -a /net/bipolar/jedidiah/mutation/output/predicted/chr${chr}_full.bed -b /net/bipolar/jedidiah/mutation/reference_data/20140520.strict_mask2.bed  | awk '{print $3"\t"$4}' > /net/bipolar/jedidiah/mutation/output/predicted/chr${chr}_full_mask.txt
+# bedtools intersect -a /net/bipolar/jedidiah/mutation/output/predicted/chr${chr}_full.bed -b /net/bipolar/jedidiah/mutation/reference_data/20140520.strict_mask2.bed  | awk '{print $3"\t"$4}' > /net/bipolar/jedidiah/mutation/output/predicted/chr${chr}_full_mask.txt

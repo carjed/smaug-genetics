1646ec7ce7ef491c4bf0a889a879f2e0dbb8c5c8
Jed Carlson <jedidiah@umich.edu>
2016-05-01 19:36:49 -0400

    annotate dnms per chr

--- data_mgmt/process_dnms.sh
+++ data_mgmt/process_dnms.sh
@@ -7,6 +7,6 @@ chr=$1
 # | sed "s/$/\t0/"
 # awk -v chr="$chr" 'BEGIN {srand()} !/^$/ { if (rand() <= .005) print chr"\t"$0"\t"0}' /net/bipolar/jedidiah/mutation/output/predicted/full/chr${chr}_full.txt > /net/bipolar/jedidiah/mutation/output/predicted/full/chr${chr}_sub.txt
 
-grep -Fwf  <(cut -f 2 /net/bipolar/jedidiah/mutation/reference_data/GoNL_DNMs2.txt) /net/bipolar/jedidiah/mutation/output/predicted/full/chr${chr}_full.txt | sed "s/$/\t1/" > /net/bipolar/jedidiah/mutation/reference_data/GoNL_DNMs2_chr${chr}_mu.txt
+grep -Fwf  <(grep "$1" /net/bipolar/jedidiah/mutation/reference_data/GoNL_DNMs2.txt | cut -f 2)  /net/bipolar/jedidiah/mutation/output/predicted/full/chr${chr}_full.txt | sed "s/$/\t1/" > /net/bipolar/jedidiah/mutation/reference_data/GoNL_DNMs2_chr${chr}_mu.txt
 
 # cat /net/bipolar/jedidiah/mutation/output/predicted/full/chr*_sub.txt > /net/bipolar/jedidiah/mutation/output/predicted/full/all_sub.txt

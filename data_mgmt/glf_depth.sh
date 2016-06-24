a6d8ab41437e17c175221b2a672a1f460c07f720
Jed Carlson <jedidiah@umich.edu>
2016-06-24 13:41:59 -0400

    process glfs

--- data_mgmt/glf_depth.sh
+++ data_mgmt/glf_depth.sh
@@ -2,6 +2,6 @@
 
 for i in /net/bipolar/bridges/sequencedata/umake/glfs/samples/chr1/1.5000000/*.glf;
 do
-  fname=basename($i)
+  fname=$basename($i)
   samtools-hybrid glfview $i  | cut -f1,2,4 > /net/bipolar/jedidiah/mutation/output/glf_depth/$fname.dp
 done

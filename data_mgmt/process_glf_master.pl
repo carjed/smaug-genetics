20bc085e738f443d1a5bbf13aab8d70c03d206be
Jed Carlson <jedidiah@umich.edu>
2016-06-30 18:05:39 -0400

    process glfs

--- data_mgmt/process_glf_master.pl
+++ data_mgmt/process_glf_master.pl
@@ -102,9 +102,9 @@ while($cflag!=1){
 
   my $logfile="$parentdir/output/glf_depth/chr$chr.data.log2";
   print "Reading $logfile\n";
-  my $logcmd="sacct -j $ID --format=JobID,State | awk 'NR>2 {print \$2}' | sort | uniq | paste -d- -s > $logfile";
+  # my $logcmd="sacct -j $ID --format=JobID,State | awk 'NR>2 {print \$2}' | sort | uniq | paste -d- -s > $logfile";
   print "Running $logcmd\n";
-  &forkExecWait($logcmd);
+  # &forkExecWait($logcmd);
 
   open my $logFH, '<', $logfile or die "can't open $logfile: $!";
 

89dd5ce0a66238dc0b4f1f7090ed0b0bd651ae1c
Jed Carlson <jedidiah@umich.edu>
2016-06-30 18:16:28 -0400

    process glfs

--- data_mgmt/process_glf_master.pl
+++ data_mgmt/process_glf_master.pl
@@ -113,7 +113,7 @@ while($cflag!=1){
     print "$line\n";
     print "$line\n";
     print "$cflag\n";
-    if($line =~ /^COMPLETED$/){
+    if($line =~ /^COMPLETE/){
       $cflag=1;
       print "VALIDATION COMPLETE\n";
       last;

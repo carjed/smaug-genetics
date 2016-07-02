6db07aa562d5da3beb3a82fff8bc7e676334ebad
Jed Carlson <jedidiah@umich.edu>
2016-07-02 17:08:30 -0400

    process glfs

--- data_mgmt/process_glf_master.pl
+++ data_mgmt/process_glf_master.pl
@@ -106,28 +106,70 @@ print "Batch job $ID queued at $datestring...\n";
 $datestring = localtime();
 print "Validation started at $datestring...\n";
 
+# my $joblog = "$parentdir/smaug-genetics/data_mgmt/slurm_joblist.$chr.txt";
+# open my $jobFH, '>', $joblog or die "can't write to $joblog: $!\n";
+
+my %statushash=();
 my $cflag=0;
 OUTER:
 while($cflag!=1){
 
-  my $logfile="$parentdir/output/glf_depth/chr$chr.data.log";
-  my $logcmd="sacct -j $ID --format=JobID,State | awk 'NR>2 {print \$2}' | sort | uniq | paste -d- -s > $logfile";
-  &forkExecWait($logcmd);
+  # my $logfile="$parentdir/output/glf_depth/chr$chr.data.log";
+  # my $logcmd="sacct -j $ID --format=JobID,State | awk 'NR>2 {print \$2}' | sort | uniq | paste -d- -s > $logfile";
+  # &forkExecWait($logcmd);
+  #
+  # open my $logFH, '<', $logfile or die "can't open $logfile: $!";
 
-  open my $logFH, '<', $logfile or die "can't open $logfile: $!";
+  for(my $i in 1:$numjobs){
+    # my $jobcmd="perl $parentdir/smaug-genetics/data_mgmt/process_glf_worker.pl --chr $chr --ind $i --chunk $chunksize --filelist $chrfilesub";
+    # print $jobFH "$jobcmd\n";
 
-  while( my $line = <$logFH>){
-    chomp($line);
-    if($line =~ /^COMPLETED$/){
+    my $grepstr = "${ID}_$i ";
+    my $status=`sacct -j $ID --format=jobid%30,state | grep '$grepstr' | awk '{print \$2}'`;
+    chomp($status);
+
+    if($status eq "COMPLETED"){
+      $statushash{$i}=1;
+    } else {
+      $statushash{$i}=0;
+      &forkExecWait($jobcmd);
+    }
+
+    my $numcompleted = sum values %statushash;
+    if($numcompleted==$numjobs){
       $cflag=1;
       print "VALIDATION COMPLETE\n";
       last OUTER;
     }
   }
 
-  close($logFH) or die "Unable to close file: $logfile $!";
+  # close($logFH) or die "Unable to close file: $logfile $!";
   sleep 30;
 }
+
+# OLD VALIDATION LOOP
+# my $cflag=0;
+# OUTER:
+# while($cflag!=1){
+#
+#   my $logfile="$parentdir/output/glf_depth/chr$chr.data.log";
+#   my $logcmd="sacct -j $ID --format=JobID,State | awk 'NR>2 {print \$2}' | sort | uniq | paste -d- -s > $logfile";
+#   &forkExecWait($logcmd);
+#
+#   open my $logFH, '<', $logfile or die "can't open $logfile: $!";
+#
+#   while( my $line = <$logFH>){
+#     chomp($line);
+#     if($line =~ /^COMPLETED$/){
+#       $cflag=1;
+#       print "VALIDATION COMPLETE\n";
+#       last OUTER;
+#     }
+#   }
+#
+#   close($logFH) or die "Unable to close file: $logfile $!";
+#   sleep 30;
+# }
 
 my %filehash=();
 my $f_dirlist = "$parentdir/output/glf_depth/chr${chr}_glf_dirlist.txt";

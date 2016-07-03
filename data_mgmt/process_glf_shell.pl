c18ca0bf1bb57e37c7994718a4c4f6c9e624f6d9
Jed Carlson <jedidiah@umich.edu>
2016-07-03 09:53:58 -0400

    process glfs

--- data_mgmt/process_glf_shell.pl
+++ data_mgmt/process_glf_shell.pl
@@ -15,7 +15,8 @@ use List::Util qw(first max maxstr min minstr reduce shuffle sum);
 use Math::Round;
 use Cwd;
 
-foreach my $i (5..18){
+my @chrs=(5..18, 20);
+foreach my $i (@chrs){
   print "Processing chr$i...\n";
   my $pgmcmd="perl /net/bipolar/jedidiah/mutation/smaug-genetics/data_mgmt/process_glf_master.pl --chr $i";
   &forkExecWait($pgmcmd);

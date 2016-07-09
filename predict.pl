2f0ee52a0ba2f0cbfb29db24fc3edbeb0e1671d9
Jedidiah Carlson <jedidiah@umich.edu>
2016-07-09 08:24:14 -0400

    Sat Jul  9 08:24:13 2016: fixing logmod

--- predict.pl
+++ predict.pl
@@ -14,6 +14,9 @@ use File::Basename;
 use File::Path qw(make_path);
 use List::Util qw(first max maxstr min minstr reduce shuffle sum);
 use Math::Round;
+use Compress::Zlib;
+use IO::Compress::Gzip;
+use IO::Uncompress::Gunzip qw($GunzipError);
 
 my $parentdir="/net/bipolar/jedidiah/mutation";
 
@@ -26,14 +29,16 @@ GetOptions('chr=s' => \$chr,
 			'bink=s' => \$bink);
 
 my $f_coefs = "$parentdir/output/logmod_data/${cat}_${bink}kb_coefs.txt";
-my $f_data = "$parentdir/output/logmod_data/chr${chr}_${cat}_m.txt";
+my $f_data = "$parentdir/output/logmod_data/chr${chr}_${cat}_m.txt.gz";
 my $outfile = "$parentdir/output/predicted/chr${chr}_${cat}_predicted.txt";
 
 # initialize covariate data
 open my $coefs, '<', $f_coefs or die "can't open $f_coefs: $!";
 
 # initialize input file
-open my $data, '<', $f_data or die "can't open $f_data: $!";
+# open my $data, '<', $f_data or die "can't open $f_data: $!";
+my $data = IO::Uncompress::Gunzip -> new($f_data)
+    or die "IO::Uncompress::Gunzip failed: $GunzipError\n";
 
 # initialize output
 open(OUT, '>', $outfile) or die "can't write to $outfile: $!\n";

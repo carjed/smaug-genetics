efab4b218c5e8fe5d155e0f697bcdcfa0d9e3dda
Jedidiah Carlson <jedidiah@umich.edu>
2016-07-14 10:07:54 -0400

    Thu Jul 14 10:07:54 2016: test ranges.r

--- R/ranges.r
+++ R/ranges.r
@@ -6,29 +6,30 @@ library(dplyr)
 sites<-read.table("/net/bipolar/jedidiah/mutation/output/logmod_data/motifs/GC_TA/dp/GC_TA_GTCCTGT_dp.txt", header=F)
 
 # Get inside/outside broad histone peak status for each site
-histCol <- function(sites, mark){
-  file <- paste0("/net/bipolar/jedidiah/mutation/reference_data/histone_marks/broad/sort.E062-", mark, ".bed")
-  feat_ranges <- bed_to_granges(file, header=F)
-  site_ranges <- GRanges(seqnames=paste0("chr",sites$V2), ranges=IRanges(start=sites$V1, end=sites$V1))
-  return(as.integer(site_ranges %within% feat_ranges))
-}
-
-# Get CpG Island status of each site
-cpgiCol <- function(sites){
-  file <- "/net/bipolar/jedidiah/mutation/reference_data/cpg_islands_sorted.bed"
-  feat_ranges <- bed_to_granges(file, header=F)
-  site_ranges <- GRanges(seqnames=paste0("chr",sites$V2), ranges=IRanges(start=sites$V1, end=sites$V1))
-  return(as.integer(site_ranges %within% feat_ranges))
-}
+# histCol <- function(sites, mark){
+#   file <- paste0("/net/bipolar/jedidiah/mutation/reference_data/histone_marks/broad/sort.E062-", mark, ".bed")
+#   feat_ranges <- bed_to_granges(file, header=F)
+#   site_ranges <- GRanges(seqnames=paste0("chr",sites$V2),
+#                          ranges=IRanges(start=sites$V1, end=sites$V1))
+#   return(as.integer(site_ranges %within% feat_ranges))
+# }
+#
+# # Get CpG Island status of each site
+# cpgiCol <- function(sites){
+#   file <- "/net/bipolar/jedidiah/mutation/reference_data/cpg_islands_sorted.bed"
+#   feat_ranges <- bed_to_granges(file, header=F)
+#   site_ranges <- GRanges(seqnames=paste0("chr",sites$V2),
+#                          ranges=IRanges(start=sites$V1, end=sites$V1))
+#   return(as.integer(site_ranges %within% feat_ranges))
+# }
 
 # Get recombination rate at each site
-rcrCol <- function(sites){
-  file <- "/net/bipolar/jedidiah/mutation/reference_data/recomb_rate.bed"
+rcrCol <- function(sites, file){
   feat_ranges <- bed_to_granges(file, header=T)
-  site_ranges <- GRanges(seqnames=paste0("chr",sites$V2), ranges=IRanges(start=sites$V1, end=sites$V1))
+  site_ranges <- GRanges(seqnames=paste0("chr",sites$V2),
+                         ranges=IRanges(start=sites$V1, end=sites$V1))
 
   indices <- findOverlaps(site_ranges, feat_ranges, type="within", select="first")
-
   indices[is.na(indices)]<-0
   ind_df<-data.frame(V1=sites$V1, V2=sites$V2, indices)
 
@@ -43,14 +44,30 @@ rcrCol <- function(sites){
   return(rates)
 }
 
+# Get lamin B1 status at each site
+# laminCol <- function(sites){
+#   file <- "/net/bipolar/jedidiah/mutation/reference_data/lamin_B1_LADS2.bed"
+#   feat_ranges <- bed_to_granges(file, header=F)
+#   site_ranges <- GRanges(seqnames=paste0("chr",sites$V2),
+#                          ranges=IRanges(start=sites$V1, end=sites$V1))
+#   return(as.integer(site_ranges %within% feat_ranges))
+# }
+
+binaryCol <- function(sites, bedfile){
+  feat_ranges <- bed_to_granges(bedfile, header=F)
+  site_ranges <- GRanges(seqnames=paste0("chr",sites$V2),
+                         ranges=IRanges(start=sites$V1, end=sites$V1))
+  return(as.integer(site_ranges %within% feat_ranges))
+}
+
 # Loop to add histone marks to site data
 hists<-c("H3K4me1", "H3K4me3", "H3K9ac", "H3K9me3", "H3K27ac", "H3K27me3", "H3K36me3")
 
 dflist <- list()
 for(i in 1:length(hists)){
   mark <- hists[i]
-  hist <- histCol(sites, mark)
-  colname <- make.names(mark)
+  file <- paste0("/net/bipolar/jedidiah/mutation/reference_data/histone_marks/broad/sort.E062-", mark, ".bed")
+  hist <- binaryCol(sites, file)
   dflist[[i]] <- hist
 }
 
@@ -58,8 +75,7 @@ df <- as.data.frame(do.call(cbind, dflist))
 names(df) <- hists
 sites<-cbind(sites, df)
 
-# Add CpG Island status
-sites$CpGI<-cpgiCol(sites)
-
-# Add recombination rate
-sites$RR<-rcrCol(sites)
+sites$CpGI <- binaryCol(sites, "/net/bipolar/jedidiah/mutation/reference_data/cpg_islands_sorted.bed")
+sites$RR <- rcrCol(sites, "/net/bipolar/jedidiah/mutation/reference_data/recomb_rate.bed")
+sites$LAMIN <- binaryCol(sites, "/net/bipolar/jedidiah/mutation/reference_data/lamin_B1_LADS2.bed")
+sites$DHS <- binaryCOl(sites, "/net/bipolar/jedidiah/mutation/reference_data/DHS.bed")

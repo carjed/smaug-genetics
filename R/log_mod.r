e7e9952d02dc044b5602d523610fa393d16e1784
Jedidiah Carlson <jedidiah@umich.edu>
2016-07-14 11:45:50 -0400

    Thu Jul 14 11:45:50 2016: fix logit functions

--- R/log_mod.r
+++ R/log_mod.r
@@ -144,7 +144,7 @@ coefdat <- data.frame(stringsAsFactors=F)
 motifs <- sort(unique(summfile1$Sequence))
 
 # coefdat <- foreach(i=1:length(motifs), .combine=rbind) %dopar% {
-coefdat <- foreach(i=1:16,
+coefdat <- foreach(i=1:4,
 	.combine=rbind,
 	.packages=c("speedglm", "bedr", "dplyr"),
 	.init=list(data.frame(), data.frame())) %dopar% {

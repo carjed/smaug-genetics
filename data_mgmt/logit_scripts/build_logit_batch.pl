a39f7fdfbb97de53d77d07c5055899f2ee91f42d
Jedidiah Carlson <jedidiah@umich.edu>
2016-07-17 10:59:20 -0400

    Sun Jul 17 10:59:20 2016: update redo exclusion list

--- data_mgmt/logit_scripts/build_logit_batch.pl
+++ data_mgmt/logit_scripts/build_logit_batch.pl
@@ -21,7 +21,7 @@ my $jobid=27532888;
 GetOptions('categ=s' => \$categ,
 			'jobid=i' => \$jobid);
 
-my $jobids=`sacct -j $jobid --format=jobid%30,jobname%30,state | grep "FAILED" | grep "test_logit" | awk 'NR>1 {print \$1}' | sed 's/${jobid}_//g'`;
+my $jobids=`sacct -j $jobid --format=jobid%30,jobname%30,state | grep "FAILED" | grep "test_logit" | awk '{print \$1}' | sed 's/${jobid}_//g'`;
 
 # print "$jobids\n";
 $jobids =~ s/\r?\n/,/g;
@@ -41,7 +41,7 @@ print OUT "#SBATCH --job-name=logit_slurm_${categ} \n";
 print OUT "#SBATCH --partition=nomosix \n";
 print OUT "#SBATCH --array=$jobids \n";
 print OUT "#SBATCH --requeue \n";
-print OUT "#SBATCH --exclude=hunt-mc05,hunt-mc06,hunt-mc07,dl3614,dl3615,dl3616,dl3617,dl3618,dl3619 \n";
+print OUT "#SBATCH --exclude=hunt-mc05,hunt-mc06,hunt-mc07,hunt-mc08,dl3614,dl3615,dl3616,dl3617,dl3618,dl3619 \n";
 # print OUT "#SBATCH --exclude=topmed,topmed2 \n";
 print OUT "#SBATCH --output=\"/net/bipolar/jedidiah/mutation/output/slurm/slurmJob-%J.out\" --error=\"/net/bipolar/jedidiah/mutation/output/slurm/slurmJob-%J.err\" \n";
 print OUT "srun Rscript log_mod.r --categ=$categ --jobid=\$SLURM_ARRAY_TASK_ID\n";

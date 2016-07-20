dac3bcc94516a36bf5927a32105824416927112a
Jedidiah Carlson <jedidiah@umich.edu>
2016-07-20 09:03:57 -0400

    Wed Jul 20 09:03:56 2016: add ERV rates

--- data_mgmt/process_dnms/anno_rate.pl
+++ data_mgmt/process_dnms/anno_rate.pl
@@ -43,7 +43,7 @@ pod2usage(0) if $help;
 pod2usage(-verbose => 2) if $man;
 
 # Initialize and hash rate table
-my $f_rates = "$parentdir/${subseq}bp_rates.txt";
+my $f_rates = "$parentdir/ERV_${subseq}bp_rates.txt";
 open my $rates, '<', $f_rates or die "can't open $f_rates: $!";
 readline($rates); #<-throws out header
 # print "Hashing rate table...\n";
@@ -58,12 +58,13 @@ while (<$rates>){
 }
 
 # my $f_positions="/net/bipolar/jedidiah/mutation/output/predicted/chr${chr}_full_mask.txt";
-my $f_positions="$parentdir/output/predicted/full/rocdat_comb_sort.txt";
+# my $f_positions="$parentdir/output/predicted/full/rocdat_comb_sort.txt";
+my $f_positions="$parentdir/output/predicted/full/rocdat_comb_7bp.txt";
 # my $f_positions="/net/bipolar/jedidiah/mutation/reference_data/uk10k_dnms_s.txt";
 open my $positions, '<', $f_positions or die "can't open $f_positions: $!";
 
 # Initialize output file
-my $outfile ="$parentdir/output/predicted/full/rocdat_comb_${subseq}bp.txt";
+my $outfile ="$parentdir/output/predicted/full/rocdat_comb2_${subseq}bp.txt";
 # my $outfile ="$parentdir/reference_data/uk10kdnms_3mer.txt";
 open(OUT, '>', $outfile) or die "can't write to $outfile: $!\n";
 # print OUT "CHR\tPOS\tAT_CG\tAT_GC\tAT_TA\tGC_AT\tGC_CG\tGC_TA\n";

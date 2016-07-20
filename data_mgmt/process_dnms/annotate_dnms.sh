bab8bd7b516f8fc40c5b5d384fccc48f626adbe9
Jedidiah Carlson <jedidiah@umich.edu>
2016-07-20 13:16:21 -0400

    Wed Jul 20 13:16:20 2016: annotate dnms

---
+++ data_mgmt/process_dnms/annotate_dnms.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+##############################################################################
+# Step 0:
+# Merges per-category predicted rates into single chromosome file, sorted by
+# position
+##############################################################################
+
+# chr=$1
+categ=$1
+
+for i in {1:22}
+do
+  grep -Fwfh  <(grep "\s$i\s" /net/bipolar/jedidiah/mutation/reference_data/DNMs/GoNL_${categ}.txt | cut -f 3)  /net/bipolar/jedidiah/mutation/output/predicted/chr${i}.${categ}.txt | awk -v categ="$categ" '{print $0"\t"1"\tcateg"}' >> /net/bipolar/jedidiah/mutation/reference_data/DNMs/GoNL_${categ}.anno.txt
+done

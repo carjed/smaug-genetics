7dbec31e2194b550758ca3687f110fe6555c5dfc
Jedidiah Carlson <jedidiah@umich.edu>
2016-07-20 13:34:16 -0400

    Wed Jul 20 13:34:16 2016: update sampling

--- data_mgmt/process_dnms/sample_mu.sh
+++ data_mgmt/process_dnms/sample_mu.sh
@@ -10,4 +10,4 @@ categ=$1
 
 # Get random selection of sites with predicted rates
 # Must modify to include category
-awk -v categ="$categ" 'BEGIN {srand()} !/^$/ { if (rand() <= .005) print chr"\t"$0"\t"0"\t"categ}' /net/bipolar/jedidiah/mutation/output/predicted/chr*.${categ}.txt > /net/bipolar/jedidiah/mutation/output/predicted/${categ}.sub.txt
+awk -v categ="$categ" 'BEGIN {srand()} !/^$/ { if (rand() <= .005) print $0"\t"0"\t"categ}' /net/bipolar/jedidiah/mutation/output/predicted/chr*.${categ}.txt > /net/bipolar/jedidiah/mutation/output/predicted/${categ}.sub.txt

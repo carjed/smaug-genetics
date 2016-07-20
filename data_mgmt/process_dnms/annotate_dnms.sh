f5a026196f8dcffa4b58f3f35140e59a834bd80a
Jedidiah Carlson <jedidiah@umich.edu>
2016-07-20 13:23:18 -0400

    Wed Jul 20 13:23:17 2016: annotate dnms

--- data_mgmt/process_dnms/annotate_dnms.sh
+++ data_mgmt/process_dnms/annotate_dnms.sh
@@ -9,7 +9,7 @@
 # chr=$1
 categ=$1
 
-for i in {1:22}
+for i in {1..22}
 do
-  grep -Fwf  <(grep "\s${i}\s" /net/bipolar/jedidiah/mutation/reference_data/DNMs/GoNL_${categ}.txt | cut -f 3)  /net/bipolar/jedidiah/mutation/output/predicted/chr${i}.${categ}.txt | awk -v categ="$categ" '{print $0"\t"1"\tcateg"}' >> /net/bipolar/jedidiah/mutation/reference_data/DNMs/GoNL_${categ}.anno.txt
+  grep -Fwf  <(grep "\s$i\s" /net/bipolar/jedidiah/mutation/reference_data/DNMs/GoNL_${categ}.txt | cut -f 3)  /net/bipolar/jedidiah/mutation/output/predicted/chr$i.${categ}.txt | awk -v categ="$categ" '{print $0"\t"1"\tcateg"}' >> /net/bipolar/jedidiah/mutation/reference_data/DNMs/GoNL_${categ}.anno.txt
 done

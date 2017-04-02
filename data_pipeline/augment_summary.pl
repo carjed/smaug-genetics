425f4e002e7bcc57bd3f0d2563de4938568d9ee7
Jed Carlson <jedidiah@umich.edu>
2017-04-01 20:51:42 -0400

    Sat Apr  1 20:51:42 2017: improve motif counting

--- data_pipeline/augment_summary.pl
+++ data_pipeline/augment_summary.pl
@@ -122,6 +122,7 @@ if($count_motifs){
 	} else {
 		$bin_flag = "bychr";
 	}
+	our @motifs;
 	&countMotifs($bin_flag);
 	# &binCounts();
 
@@ -284,7 +285,7 @@ sub countMotifs{
 	my $bin;
 
 	print BIN "CHR\tBIN\tMOTIF\tCOUNT\n";
-	our @motifs;
+
 	if($bybin eq "bin"){
 		for my $i (0 .. $numbins-1) {
 			@motifs=(substr($seq, $i*$binwidth, $binwidth)=~/(?=([ACGT]{$subseq}))/g);

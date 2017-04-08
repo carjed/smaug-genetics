50814890b2e2ffba742feed1e0b95455c02d1378
Jed Carlson <jedidiah@umich.edu>
2017-04-08 17:50:51 -0400

    Sat Apr  8 17:50:51 2017: test new sample_mu procedure

--- data_mgmt/process_predicted/sample_mu.pl
+++ data_mgmt/process_predicted/sample_mu.pl
@@ -84,12 +84,13 @@ foreach my $categ (@categs){
 
       my $localseq = substr($seq, $dnmpos-$adj-1, $subseq);
       my $seqp = getMotif($localseq, $adj);
-      print "DNM line: $_\n";
+      print "DNM pos: $dnmpos\n";
       my $rateline = `grep -w $dnmpos $predfile`;
       chomp $rateline;
       # print "grep result: $rateline\n";
       my @ratelinearr = split(/\t/, $rateline);
       if($rateline=~/$dnmpos/){
+        print "$rateline contains DNM site: $dnmpos\n";
         print $outFH "$rateline\t1\t$categ\t$seqp\n";
       }
     }

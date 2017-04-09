5f8602079a0f8a59ca537a3227a002a106bc5cda
Jed Carlson <jedidiah@umich.edu>
2017-04-09 18:47:45 -0400

    Sun Apr  9 18:47:45 2017: bugfix

--- data_mgmt/data_prep/vcf_to_summary.pl
+++ data_mgmt/data_prep/vcf_to_summary.pl
@@ -87,7 +87,7 @@ my $script = 1;
 if ($script==1){
   my @vcfs = File::Find::Rule->file()
                             ->name("*.vcf.gz")
-                            ->maxdepth(1)
+                            ->maxdepth(0)
                             ->in($vcfdir);
 
 	foreach my $file (@vcfs) {

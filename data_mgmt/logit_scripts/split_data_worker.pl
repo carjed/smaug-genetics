97ad97f62ded867a225d1d449bc0c318b95e54d6
Jed Carlson <jedidiah@umich.edu>
2017-04-12 12:55:53 -0400

    Wed Apr 12 12:55:53 2017: split on motif substr

--- data_mgmt/logit_scripts/split_data_worker.pl
+++ data_mgmt/logit_scripts/split_data_worker.pl
@@ -25,12 +25,13 @@ my $catind = $ARGV[0]-1;
 my @categs = qw( AT_CG AT_GC AT_TA GC_AT GC_CG GC_TA );
 my $categ = $categs[$catind];
 
-make_path("$parentdir/output/logmod_data/motifs2/$categ");
+my $outpath = "$parentdir/output/logmod_data/motifs2/$categ";
+make_path($outpath);
 
 foreach my $chr (1..22){
 
   my $fullfile = "$parentdir/output/logmod_data/chr${chr}_${categ}_full.txt.gz";
-  my $subcmd = "zcat $fullfile | awk '{print >> \"$parentdir/output/logmod_data/motifs/$categ/${categ}_\" substr(\$3, 1, 7) \".txt\"}' &";
+  my $subcmd = "zcat $fullfile | awk '{print >> \"$outpath/${categ}_\" substr(\$3, 1, 7) \".txt\"}' &";
 
   forkExecWait($subcmd);
 }

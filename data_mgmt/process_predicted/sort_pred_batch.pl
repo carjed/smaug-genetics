99f63db963bd21f47fe93c7a60d163e21e7651ca
Jed Carlson <jedidiah@umich.edu>
2017-06-14 21:43:58 -0400

    Wed Jun 14 21:43:57 2017: convert sort_pred_batch to grid

--- data_mgmt/process_predicted/sort_pred_batch.pl
+++ data_mgmt/process_predicted/sort_pred_batch.pl
@@ -30,25 +30,29 @@ my $slurmdir = "$parentdir/output/slurm/$today";
 
 my @categs = qw( AT_CG AT_GC AT_TA GC_AT GC_CG GC_TA );
 
-foreach my $categ (@categs){
-  my $jobcmd="${categ}_sort_pred";
-  my $builddatbatch = "$parentdir/slurm/$jobcmd.txt";
+# foreach my $categ (@categs){
+  # my $jobcmd="${categ}_sort_pred";
+  my $builddatbatch = "$parentdir/slurm/sort_pred_batch.txt";
   open my $mdFH, '>', $builddatbatch or die "can't write to $builddatbatch: $!\n";
-  print $mdFH "#!/bin/sh \n";
+  print $mdFH "#!/bin/bash \n";
+  print $mdFH "#SGRIDBATCH CAT='AT_CG' 'AT_GC' 'AT_TA' 'GC_AT' 'GC_CG' 'GC_TA'\n";
+  print $mdFH "#SGRIDBATCH INDEX=\$(seq 1 22)\n";
   print $mdFH "#SBATCH --mail-type=FAIL \n";
   print $mdFH "#SBATCH --mail-user=$email \n";
   print $mdFH "#SBATCH --ntasks=1 \n";
   print $mdFH "#SBATCH --mem=4000 \n";
   print $mdFH "#SBATCH --time 2:00:00 \n";
-  print $mdFH "#SBATCH --job-name=$jobcmd \n";
-  print $mdFH "#SBATCH --partition=nomosix \n";
-  print $mdFH "#SBATCH --array=1-22 \n";
+  print $mdFH "#SBATCH --job-name=sort_pred \n";
+  print $mdFH "#SBATCH --partition=bipolar \n";
+  # print $mdFH "#SBATCH --array=1-22 \n";
   print $mdFH "#SBATCH --requeue \n";
+  print $mdFH "export STDOUT=\"$slurmdir/chr\$INDEX.\$CAT.out\"\n";
+  print $mdFH "export STDERR=\"$slurmdir/chr\$INDEX.\$CAT.err\"\n";
   # print $mdFH "#SBATCH --exclude=psoriasis-mc01,psoriasis-mc02 \n";
-  print $mdFH "#SBATCH --output=\"$slurmdir/slurmJob-%J.out\" --error=\"$slurmdir/slurmJob-%J.err\" \n";
-  print $mdFH "srun cat $parentdir/output/predicted/$categ/chr\${SLURM_ARRAY_TASK_ID}/* | sort -V -k2 > $parentdir/output/predicted/chr\${SLURM_ARRAY_TASK_ID}.$categ.txt";
+  # print $mdFH "#SBATCH --output=\"$slurmdir/slurmJob-%J.out\" --error=\"$slurmdir/slurmJob-%J.err\" \n";
+  print $mdFH "srun cat $parentdir/output/predicted/\$CAT/chr\$INDEX/* | sort -V -k2 > $parentdir/output/predicted/chr\$INDEX.\$CAT.txt";
   close($mdFH) or die "Unable to close file: $builddatbatch $!";
 
-  my $slurmcmd="sbatch $builddatbatch";
+  my $slurmcmd="gridbatch $builddatbatch";
   forkExecWait($slurmcmd);
-}
+# }
